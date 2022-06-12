9609c8e414eecc95f05b8510245bbe7807a6846d
贺文杰 <hewenjie@51shebao.com>
2022-06-12 17:10:43 +0800

    提交类型模块注释-2

--- src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
+++ src/main/java/org/apache/ibatis/type/TypeAliasRegistry.java
@@ -33,12 +33,21 @@ import org.apache.ibatis.io.ResolverUtil;
 import org.apache.ibatis.io.Resources;
 
 /**
+ * 类型与别名的注册表。通过别名，我们在 Mapper XML 中的 resultType 和 parameterType 属性，直接使用，而不用写全类名
  * @author Clinton Begin
  */
 public class TypeAliasRegistry {
 
+  /**
+   * 类型与别名的映射。
+   */
   private final Map<String, Class<?>> TYPE_ALIASES = new HashMap<String, Class<?>>();
 
+  /**
+   * 初始化默认的类型与别名
+   *
+   * 另外，在 {@link org.apache.ibatis.session.Configuration} 构造方法中，也有默认的注册
+   */
   public TypeAliasRegistry() {
     registerAlias("string", String.class);
 
@@ -108,59 +117,90 @@ public class TypeAliasRegistry {
         return null;
       }
       // issue #748
+      // <1> 转换成小写
       String key = string.toLowerCase(Locale.ENGLISH);
       Class<T> value;
+      // <2.1> 首先，从 TYPE_ALIASES 中获取
       if (TYPE_ALIASES.containsKey(key)) {
         value = (Class<T>) TYPE_ALIASES.get(key);
+        // <2.2> 其次，直接获得对应类
       } else {
         value = (Class<T>) Resources.classForName(string);
       }
       return value;
-    } catch (ClassNotFoundException e) {
+    } catch (ClassNotFoundException e) { // <2.3> 异常
       throw new TypeException("Could not resolve type alias '" + string + "'.  Cause: " + e, e);
     }
   }
 
+  /**
+   * 注册指定包下的别名与类的映射
+   *
+   * @param packageName 指定包
+   */
   public void registerAliases(String packageName){
     registerAliases(packageName, Object.class);
   }
 
+  /**
+   * 注册指定包下的别名与类的映射。另外，要求类必须是 {@param superType} 类型（包括子类）。
+   *
+   * @param packageName 指定包
+   * @param superType 指定父类
+   */
   public void registerAliases(String packageName, Class<?> superType){
-    ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<Class<?>>();
+    // 获得指定包下的类门
+    ResolverUtil<Class<?>> resolverUtil = new ResolverUtil<>();
     resolverUtil.find(new ResolverUtil.IsA(superType), packageName);
     Set<Class<? extends Class<?>>> typeSet = resolverUtil.getClasses();
-    for(Class<?> type : typeSet){
+    // 遍历，逐个注册类型与别名的注册表
+    for (Class<?> type : typeSet) {
       // Ignore inner classes and interfaces (including package-info.java)
       // Skip also inner classes. See issue #6
-      if (!type.isAnonymousClass() && !type.isInterface() && !type.isMemberClass()) {
+      if (!type.isAnonymousClass() // 排除匿名类
+              && !type.isInterface()  // 排除接口
+              && !type.isMemberClass()) { // 排除内部类
         registerAlias(type);
       }
     }
   }
 
   public void registerAlias(Class<?> type) {
+    // <1> 默认为，简单类名
     String alias = type.getSimpleName();
+    // <2> 如果有注解，使用注册上的名字
     Alias aliasAnnotation = type.getAnnotation(Alias.class);
     if (aliasAnnotation != null) {
       alias = aliasAnnotation.value();
-    } 
+    }
+    // <3> 注册类型与别名的注册表
     registerAlias(alias, type);
   }
 
+  /**
+   * * `<1>` 处，将别名转换成**小写**。这样的话，无论我们在 Mapper XML 中，写 `String` 还是 `string` 甚至是 `STRING` ，都是对应的 String 类型。
+   * * `<2>` 处，如果已经注册，并且类型不一致，说明有冲突，抛出 TypeException 异常。
+   * * `<3>` 处，添加到 `TYPE_ALIASES` 中。
+   * @param alias
+   * @param value
+   */
   public void registerAlias(String alias, Class<?> value) {
     if (alias == null) {
       throw new TypeException("The parameter alias cannot be null");
     }
     // issue #748
+    // <1> 转换成小写
     String key = alias.toLowerCase(Locale.ENGLISH);
-    if (TYPE_ALIASES.containsKey(key) && TYPE_ALIASES.get(key) != null && !TYPE_ALIASES.get(key).equals(value)) {
+    if (TYPE_ALIASES.containsKey(key) && TYPE_ALIASES.get(key) != null && !TYPE_ALIASES.get(key).equals(value)) { // <2> 冲突，抛出 TypeException 异常
       throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + TYPE_ALIASES.get(key).getName() + "'.");
     }
+    // <3>
     TYPE_ALIASES.put(key, value);
   }
 
   public void registerAlias(String alias, String value) {
     try {
+      // Resources.classForName(value) 通过类名的字符串，获得对应的类。
       registerAlias(alias, Resources.classForName(value));
     } catch (ClassNotFoundException e) {
       throw new TypeException("Error registering type alias "+alias+" for "+value+". Cause: " + e, e);
