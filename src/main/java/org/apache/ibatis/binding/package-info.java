d19f84ef3a5369cdf9803611a199f9ee01e619b8
hwj <renshen052@foxmail.com>
2022-06-21 10:09:14 +0800

    提交bingding模块注释

--- src/main/java/org/apache/ibatis/binding/package-info.java
+++ src/main/java/org/apache/ibatis/binding/package-info.java
@@ -14,6 +14,11 @@
  *    limitations under the License.
  */
 /**
+ * 在调用 SqlSession 相应方法执行数据库操作时，需要指定映射文件中定义的 SQL 节点，
+ * 如果出现拼写错误，我们只能在运行时才能发现相应的异常。
+ * 为了尽早发现这种错误，MyBatis 通过 Binding 模块，
+ * 将用户自定义的 Mapper 接口与映射配置文件关联起来，
+ * 系统可以通过调用自定义 Mapper 接口中的方法执行相应的 SQL 语句完成数据库操作，从而避免上述问题。
  * Bings mapper interfaces with mapped statements
  */
 package org.apache.ibatis.binding;
